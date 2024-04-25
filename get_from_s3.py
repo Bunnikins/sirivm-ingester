5caf65c7ad8728141446986fa5a5be03f3a22f5a
Pete Cridland <peter.cridland@kpmg.co.uk>
2024-04-25 19:41:27 +0100

    sqlite optimisatopn

--- get_from_s3.py
+++ get_from_s3.py
@@ -5,9 +5,9 @@ from concurrent.futures import ThreadPoolExecutor
 import zipfile
 from io import BytesIO
 from datetime import datetime, timezone, timedelta
-from transform_load import validate_data,extract_data,parse_xml,init_db, insert_data, init_cache
+from transform_load_shared_conn import validate_data, parse_xml, init_db, insert_data
 import sqlite3
-from pypeln import process, thread, sync
+from pypeln import process, thread, sync, task
 
 S3_BUCKET = environ.get('S3_BUCKET')
 HOURS_TO_EXTRACT = float(environ.get('HOURS_TO_EXTRACT'))
@@ -17,41 +17,55 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 db_path = f'vehicles_{timestamp}.db'
 
-s3 = client('s3')
-
 def get_object(key):
     '''Download file from s3 into BytesIO and unzip file to memory'''
     try:
-        logging.info(f"Downloading file {key['Key']} from S3")
+        logging.debug(f"Downloading file {key['Key']} from S3")
         obj = s3.get_object(Bucket=S3_BUCKET, Key=key['Key'])
         zip_file = zipfile.ZipFile(BytesIO(obj['Body'].read()))
-        return zip_file.read(zip_file.namelist()[0])
+        return {
+            "id": key['Key'],
+            "data": zip_file.read(zip_file.namelist()[0])
+        }
     except Exception as e:
-        print('Error:', e)
+        logging.error(f'Failed to download {key["Key"]}', e)
 
 def parse_data_step(key_data):
-        logging.info('Parsing data')
-        return parse_xml(key_data, source_type='string')
+    try:
+        logging.debug(f'Parsing data for {key_data["id"]}')
+        return {
+            "id": key_data['id'],
+            "data": parse_xml(key_data['data'], source_type='string'),
+        }
+    except Exception as e:
+        logging.error(f'Failed to parse {key_data["id"]}', e)
+
 
 def validate_data_step(parsed_data):
-        logging.info('Validating data')
-        return validate_data(parsed_data)
+    try:
+        logging.debug(f'Validating data for {parsed_data["id"]}')
+        return {
+            "id": parsed_data['id'],
+            "data": validate_data(parsed_data['data'])
+        }
+    except Exception as e:
+        logging.error(f'Failed to validate {parsed_data["id"]}', e)
 
 def insert_data_step(validated_data):
     try:
-        logging.info('Creating cache')
-        cache = init_cache(db_path)  # Initialize the cache
-        logging.info('Inserting Data')
-        insert_data(db_path, validated_data, cache)  # Pass the cache to insert_data
+        logging.info(f'Inserting data for {validated_data["id"]}')
+        insert_data(conn, validated_data['data'])
+        return validated_data['id']
     except Exception as e:
-        logging.error("Error processing data", e)
+        logging.error(f'Failed to insert {validated_data["id"]}', e)
     # logging.info("Data processing and insertion completed.")
 
 if __name__ == '__main__':
     try:
+        s3 = client('s3')
+        conn = init_db(db_path)
         keys = []
         earliest_time = datetime.now(timezone.utc) - timedelta(hours=HOURS_TO_EXTRACT)
-        init_db(db_path)
         paginator = s3.get_paginator('list_objects_v2')
         operation_parameters = {'Bucket': S3_BUCKET,
                                 'Prefix': 'sirivm_20'}
@@ -61,11 +75,13 @@ if __name__ == '__main__':
                 if key['LastModified'] > earliest_time:
                     keys.append(key)
 
-        logging.info(f'Found {str(len(keys))} keys more than x hours old')
+        logging.info(f'Found {str(len(keys))} keys more than {str(HOURS_TO_EXTRACT)} hours old')
         stage = thread.map(get_object, keys, workers=5, maxsize=5)
         stage = process.map(parse_data_step, stage, workers=5, maxsize=5)
         stage = process.map(validate_data_step, stage, workers=5, maxsize=5)
-        stage = thread.map(insert_data_step, stage, workers=1, maxsize=5)
+        stage = sync.map(insert_data_step, stage, workers=1, maxsize=5)
         out=list(stage)
+        conn.close()
+        logging.info(f'parsed {str(len(out))} of {str(len(keys))} keys')
     except Exception as e:
         logging.error("Error fetching keys from S3", e)
